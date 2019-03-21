1da945afa1bd20e1964addd8c150adf39f54aa99
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 15:36:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,8 +5,8 @@ def square_array(array)
     square_value = array_value ** 2
     new_array << square_value
   end
-  return new_array  
+  # return new_array  
 end
 
-the_array = [1, 2, 3, 4, 5]
+the_array = [3, 4, 5, 6, 10]
 puts square_array(the_array)
