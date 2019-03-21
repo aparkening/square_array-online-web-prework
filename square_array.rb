02ff158c7366d39772cdb9d85407dfee20c6742d
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 15:40:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 # Take an array, square each value, and return another array with the squared values
 def square_array(array)
+  array.collect { |array_value| array_value ** 2 }
+end
+
+
+# Long form version. Take an array, square each value, and return another array with the squared values
+def square_array(array)
   new_array = [ ]
   array.each do |array_value|
     square_value = array_value ** 2
